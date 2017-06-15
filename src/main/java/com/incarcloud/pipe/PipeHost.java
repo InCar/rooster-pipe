9c9d68006826036e982d610212f06657ec64e661
XGH <ghxiong@incarcloud.com>
2017-06-15 13:12:08 +0800

    PipeHost

---
+++ src/main/java/com/incarcloud/pipe/PipeHost.java
@@ -0,0 +1,23 @@
+package com.incarcloud.pipe;
+
+import com.incarcloud.rooster.mq.IBigMQ;
+
+public class PipeHost {
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public IBigMQ getBigMQ(){
+        return null;
+    }
+
+    public void setBIgMQ(IBigMQ bigMQ){
+
+    }
+
+}
